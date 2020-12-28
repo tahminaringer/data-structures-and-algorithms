3bcdcc7b0450c98fcaf287448b1a9240e1f38732
Tahmina <tahmina.ringer@gmail.com>
2020-12-27 23:05:11 -0800

    finished binary trees

---
+++ javascript/code-challenges/tree/__tests__/tree.test.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const {Node, BinarySearchTree} = require('../tree');
+
+describe('Bianary Trees', () => {
+  describe('Node Constructor', () => {
+    it('Can successfully instantiates a new node', () => {
+      let node = new Node(8);
+      expect(node.left).toBeNull();
+      expect(node.right).toBeNull();
+      expect(node.value).toBe(8);
+    });
+  });
+
+  describe('Binary Search Tree', () => {
+    it('Can instantiate an empty tree', () => {
+      let tree = new BinarySearchTree();
+      expect(tree.root).toBeNull();
+    });
+    it('add() can instantiate a new node', () => {
+      let tree = new BinarySearchTree();
+      tree.add(3);
+      expect(tree.root.value).toBe(3);
+    });
+    it('Can successfully add a left child and right child to a single root node', () => {
+      let tree = new BinarySearchTree();
+      tree.add(3);
+      tree.add(1);
+      tree.add(5);
+      expect(tree.root.value).toBe(3);
+      expect(tree.root.left.value).toBe(1);
+      expect(tree.root.right.value).toBe(5);
+    });
+  });
+
+  describe('Binary Tree', () => {
+    it('successfully return null on BinaryTree methods if called on an empty tree', () => {
+      let tree = new BinarySearchTree();
+      let preOrder = tree.preOrder();
+      let inOrder = tree.inOrder();
+      let postOrder = tree.postOrder();
+      expect(preOrder).toBeNull();
+      expect(inOrder).toBeNull();
+      expect(postOrder).toBeNull();
+    });
+
+    it('Can successfully return a collection from a preorder traversal', () => {
+      let tree = new BinarySearchTree();
+      tree.add(4);
+      tree.add(2);
+      tree.add(6);
+      tree.add(1);
+      tree.add(3);
+      tree.add(5);
+      let preOrder = tree.preOrder();
+      expect(preOrder).toEqual([4, 2, 1, 3, 6, 5]);
+    });
+    it('Can successfully return a collection from a inorder traversal', () => {
+      let tree = new BinarySearchTree();
+      tree.add(4);
+      tree.add(2);
+      tree.add(6);
+      tree.add(1);
+      tree.add(3);
+      tree.add(5);
+      let inOrder = tree.inOrder();
+      expect(inOrder).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+    it('Can successfully return a collection from a postorder traversal', () => {
+      let tree = new BinarySearchTree();
+      tree.add(4);
+      tree.add(2);
+      tree.add(6);
+      tree.add(1);
+      tree.add(3);
+      tree.add(5);
+      let postOrder = tree.postOrder();
+      expect(postOrder).toStrictEqual([1, 3, 2, 5, 6, 4]);
+    });
+    it('Successfully returns true with method contains()', () => {
+      let tree = new BinarySearchTree();
+      tree.add(4);
+      tree.add(2);
+      tree.add(6);
+      tree.add(1);
+      tree.add(3);
+      tree.add(5);
+      let postOrder = tree.contains(4);
+      expect(postOrder).toBe(true);
+    });
+    it('Successfully returns false with method contains() on value that does not exist', () => {
+      let tree = new BinarySearchTree();
+      tree.add(4);
+      tree.add(2);
+      tree.add(6);
+      tree.add(1);
+      tree.add(3);
+      tree.add(5);
+      let postOrder = tree.contains(7);
+      expect(postOrder).toBe(false);
+    });
+  });
+});
+
+
---
+++ javascript/code-challenges/tree/tree.js
@@ -0,0 +1,126 @@
+'use strict';
+
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+
+  preOrder() {
+    let array = [];
+    let currentNode = this.root;
+
+    if(!currentNode) {
+      return null;
+    } else {
+      let preOrderTraversal = (currentNode) => {
+        array.push(currentNode.value);
+        if(currentNode.left) { preOrderTraversal(currentNode.left); } else { return null; }
+        if(currentNode.right) { preOrderTraversal(currentNode.right); } else { return null; }
+      };
+      preOrderTraversal(currentNode);
+      return array;
+    }
+  }
+
+
+  inOrder() {
+    let array = [];
+    let currentNode = this.root;
+
+    if(!currentNode) {
+      return null;
+    } else {
+      let inOrderTraversal = (currentNode) => {
+        if(currentNode.left) { inOrderTraversal(currentNode.left); }
+        array.push(currentNode.value);
+        if(currentNode.right) { inOrderTraversal(currentNode.right); }
+      };
+      inOrderTraversal(currentNode);
+      return array;
+    }
+  }
+
+  postOrder() {
+    let array = [];
+    let currentNode = this.root;
+
+    if(!currentNode) {
+      return null;
+    } else {
+      let postOrderTraversal = (currentNode) => {
+        if(currentNode.left) { postOrderTraversal(currentNode.left); }
+        if(currentNode.right) { postOrderTraversal(currentNode.right); }
+        array.push(currentNode.value);
+      };
+      postOrderTraversal(currentNode);
+      return array;
+    }
+  }
+
+}
+
+class BinarySearchTree extends BinaryTree {
+
+  add(value) {
+    let currentNode = new Node(value);
+
+    if(!this.root){
+      this.root = currentNode;
+    } else {
+      let addNode = (node, currentNode) => {
+        if(currentNode.value < node.value){//if current node is less than node go left and traverse else skip to right side
+          if(node.left === null) {//if left node equals null attach currentNode
+            node.left = currentNode;//connect currentNode to tree
+          } else {
+            addNode(node.left, currentNode);//if node exists continue until null
+          }
+        } else {
+          if(node.right === null) {//if left node equals null attach currentNode
+            node.right = currentNode;//connect currentNode to tree
+          } else {
+            addNode(node.right, currentNode);//if node exists continue until null
+          }
+        }
+      };
+      addNode(this.root, currentNode);
+    }
+  }
+
+  contains(value) {
+    if(!this.root){
+      return false;
+    } else {
+      let containsNode = (node, value) => {
+        console.log('node, value ======', node.value, value);
+        if(value < node.value){//if current node is less than node go left and traverse else skip to right side
+          if(node.left === null) {//if left node equals null attach currentNode
+            return false;
+          } else {
+            containsNode(node.left, value);//if node exists continue until null
+          }
+        } else if (value > node.value) {
+          if(node.right === null) {//if left node equals null attach currentNode
+            return false;//connect currentNode to tree
+          } else {
+            containsNode(node.right, value);//if node exists continue until null
+          }
+        } else { // if currentNode is equal to node return true
+          return true;
+        }
+        return false;
+      };
+      return containsNode(this.root, value);
+    }
+  }
+}
+
+module.exports = {Node, BinaryTree, BinarySearchTree};
