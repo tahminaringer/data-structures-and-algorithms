737285152300e185511850957ece89349ca51a31
Tahmina <tahmina.ringer@gmail.com>
2021-01-16 12:33:36 -0800

    updated README

---
+++ javascript/code-challenges/hashTable/README.md
@@ -0,0 +1,29 @@
+# Hash Table
+
+## Code Challenge 30
+
+Authors: Tahmina Ringer, Mariko Alvarado, Ricardo Barcenas, Simon Panek, Stacy Burris, Ashley Moore & Garret Cintron
+
+- Created a hash table using single-responsibility principlewith a Node class, LinkedList,and implimented methods for add, get, contains and hash.
+
+## Approach & Efficiency
+
+I wrote my code first and then wrote tests to validate the code.
+
+## Big O
+
+Size - O(n)
+Space - O(1)
+
+## Testing
+
+Write tests to prove the following functionality:
+
+### Challenge 30 testing
+
+- Adding a key/value to your hashtable results in the value being in the data structure
+- Retrieving based on a key returns the value stored
+- Successfully returns null for a key that does not exist in the hashtable
+- Successfully handle a collision within the hashtable
+- Successfully retrieve a value from a bucket within the hashtable that has a collision
+- Successfully hash a key to an in-range value
--- javascript/code-challenges/hashTable/__tests__/hash-table.test.js
+++ javascript/code-challenges/hashTable/__tests__/hash-table.test.js
@@ -12,14 +12,23 @@ describe('Hash Map', () => {
     newMap.set('narwal', 'noodles');
     expect(newMap.get('dog')).toEqual('soup');
   });
-  it('it should add a key and value to the hash map', () => {
+  it('it should take in a key and return a boolean if key exists in table', () => {
     let newMap = new HashMap(3);
     newMap.set('dog', 'soup');
     newMap.set('unicorn', 'stew');
     newMap.set('possum', 'pie');
     newMap.set('turtle', 'tacos');
     newMap.set('narwal', 'noodles');
+    newMap.set('cat', 'cake');
+    newMap.set('ladybug', 'wings');
+    newMap.set('hamster', 'toes');
     expect(newMap.contains('turtle')).toBe(true);
+    expect(newMap.contains('dog')).toBe(true);
+    expect(newMap.contains('unicorn')).toBe(true);
+    expect(newMap.contains('narwal')).toBe(true);
+    expect(newMap.contains('cat')).toBe(true);
+    expect(newMap.contains('ladybug')).toBe(true);
+    expect(newMap.contains('hamster')).toBe(true);
     expect(newMap.contains('turkey')).toBe(false);
   });
 });
