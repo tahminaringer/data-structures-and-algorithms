1ac449c841387c63e9bb4e9bcedc54d556aa6395
Tahmina <tahmina.ringer@gmail.com>
2021-06-04 18:25:33 -0700

    solve challenge to pass all tests

--- leetcode/rectangleOverlap/rectagleOverlap.js
+++ leetcode/rectangleOverlap/rectagleOverlap.js
@@ -23,3 +23,17 @@ var isRectangleOverlap = function(rec1, rec2) {
 };
 
 isRectangleOverlap();
+
+var isRectangleOverlap = function(rec1, rec2) {
+  if (rec1[0] === rec1[2] ||
+    rec1[1] === rec1[3] ||
+    rec2[0] === rec2[2] ||
+    rec2[1] === rec2[3] ||
+    rec1[2] <= rec2[0] ||
+    rec1[0] >= rec2[2] ||
+    rec1[3] <= rec2[1] ||
+    rec1[1] >= rec2[3] ) {
+    return false;
+  }
+  return true;
+};
