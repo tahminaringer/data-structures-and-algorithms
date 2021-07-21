d36822167f0eef1a9820a86578b79554ff33ce57
Tahmina <tahmina.ringer@gmail.com>
2021-07-20 23:20:00 -0700

    solved two sum

---
+++ leetcode/twoSum/twoSum.js
@@ -0,0 +1,10 @@
+var twoSum = function (nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] >= 0) {
+      return [map[nums[i]], i];
+    }
+    map[target - nums[i]] = i
+  }
+};
